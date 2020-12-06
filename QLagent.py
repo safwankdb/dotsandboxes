cdf66f3b4896abc1aeab51499f15abdb5728df65
safwankdb <kdbeatbox@gmail.com>
2020-12-06 13:15:18 +0530

    minor changes in QL agent

--- QLagent.py
+++ QLagent.py
@@ -21,37 +21,17 @@ games = {}
 agentclass = None
 
 
-class DotsAndBoxesAgent:
-    """Example Dots and Boxes agent implementation base class.
-    It returns a random next move.
-
-    A DotsAndBoxesAgent object should implement the following methods:
-    - __init__
-    - add_player
-    - register_action
-    - next_action
-    - end_game
-
-    This class does not necessarily use the best data structures for the
-    approach you want to use.
-    """
+class QLEnv:
 
     def __init__(self, player, nb_rows, nb_cols, timelimit):
-        """Create Dots and Boxes agent.
-
-        :param player: Player number, 1 or 2
-        :param nb_rows: Rows in grid
-        :param nb_cols: Columns in grid
-        :param timelimit: Maximum time allowed to send a next action.
-        """
         self.timelimit = timelimit
         self.ended = False
         self.nb_rows = nb_rows
         self.nb_cols = nb_cols
         rows = []
-        for ri in range(nb_rows + 1):
+        for _ in range(nb_rows + 1):
             columns = []
-            for ci in range(nb_cols + 1):
+            for _ in range(nb_cols + 1):
                 columns.append({"v": 0, "h": 0})
             rows.append(columns)
         self.cells = rows
@@ -66,7 +46,6 @@ class DotsAndBoxesAgent:
             score = score[::-1]
         self.reward = score[0] - self.score[0] - score[1] + self.score[1]
         self.score = score
-        print(self.reward)
 
 
     def update_state(self, update_prev=False):
@@ -91,44 +70,28 @@ class DotsAndBoxesAgent:
                 i += 1
         if update_prev:
             self.prev_state = self.state.copy()
-        print(self.state)
 
     def register_action(self, row, column, orientation, player):
-        """Register action played in game.
-
-        :param row:
-        :param columns:
-        :param orientation: "v" or "h"
-        :param player: 1 or 2
-        """
         self.cells[row][column][orientation] = player
         self.update_state(player==self.player)
 
     def next_action(self):
-        """Return the next action this agent wants to perform.
-
-        In this example, the function implements a random move. Replace this
-        function with your own approach.
-
-        :return: (row, column, orientation)
-        """
         logger.info("Computing next move (grid={}x{}, player={})"
                     .format(self.nb_rows, self.nb_cols, self.player))
         # Random move
-        free_lines = []
-        for ri in range(len(self.cells)):
-            row = self.cells[ri]
-            for ci in range(len(row)):
-                cell = row[ci]
-                if ri < (len(self.cells) - 1) and cell["v"] == 0:
-                    free_lines.append((ri, ci, "v"))
-                if ci < (len(row) - 1) and cell["h"] == 0:
-                    free_lines.append((ri, ci, "h"))
+        free_lines = [i for i in range(len(self.state)) if self.state[i]==0]
         if len(free_lines) == 0:
-            # Board full
             return None
-        movei = random.randint(0, len(free_lines) - 1)
-        r, c, o = free_lines[movei]
+        movei = random.choice(free_lines)
+        if movei < (self.nb_cols+1)*self.nb_rows:
+            o = 'v'
+            r = movei // (self.nb_cols+1)
+            c = movei %  (self.nb_cols+1)
+        else:
+            movei -= (self.nb_cols+1)*self.nb_rows
+            o = 'h'
+            r = movei // (self.nb_cols)
+            c = movei %  (self.nb_cols)
         return r, c, o
 
     def end_game(self):
@@ -137,7 +100,6 @@ class DotsAndBoxesAgent:
         elif self.score[0] < self.score[1]:
             self.reward += -1000
         self.ended = True
-        print(self.reward)
 
 
 
@@ -145,81 +107,64 @@ class DotsAndBoxesAgent:
 
 async def handler(websocket, path):
     logger.info("Start listening")
-    game = None
     # msg = await websocket.recv()
-    try:
-        async for msg in websocket:
-            logger.info("< {}".format(msg))
-            try:
-                msg = json.loads(msg)
-            except json.decoder.JSONDecodeError as err:
-                logger.error(err)
-                return False
-            game = msg["game"]
-            answer = None
-            if msg["type"] == "start":
-                # Initialize game
-                if msg["game"] in games:
-                    games[msg["game"]].add_player(msg["player"])
-                else:
-                    nb_rows, nb_cols = msg["grid"]
-                    games[msg["game"]] = agentclass(msg["player"],
-                                                    nb_rows,
-                                                    nb_cols,
-                                                    msg["timelimit"])
-                if msg["player"] == 1:
-                    # Start the game
-                    nm = games[game].next_action()
-                    # print('nm = {}'.format(nm))
-                    if nm is None:
-                        # Game over
-                        logger.info("Game over")
-                        continue
-                    r, c, o = nm
-                    answer = {
-                        'type': 'action',
-                        'location': [r, c],
-                        'orientation': o
-                    }
-                else:
-                    # Wait for the opponent
-                    answer = None
-
-            elif msg["type"] == "action":
-                # An action has been played
-                r, c = msg["location"]
-                o = msg["orientation"]
-                games[game].register_action(r, c, o, msg["player"])
-                if msg["nextplayer"] == games[game].player:
-                    games[msg["game"]].update_score(msg['score'])
-                    # Compute your move
-                    nm = games[game].next_action()
-                    if nm is None:
-                        # Game over
-                        logger.info("Game over")
-                        continue
-                    nr, nc, no = nm
-                    answer = {
-                        'type': 'action',
-                        'location': [nr, nc],
-                        'orientation': no
-                    }
-                else:
-                    answer = None
-
-            elif msg["type"] == "end":
-                # End the game
-                games[msg["game"]].end_game()
-                answer = None
+    async for msg in websocket:
+        logger.info("< {}".format(msg))
+        msg = json.loads(msg)
+        answer = None
+        if msg["type"] == "start":
+            # Initialize game
+            nb_rows, nb_cols = msg["grid"]
+            agent = QLEnv(msg["player"],
+                                            nb_rows,
+                                            nb_cols,
+                                            msg["timelimit"])
+            if msg["player"] == 1:
+                # Start the game
+                nm = agent.next_action()
+                if nm is None:
+                    # Game over
+                    logger.info("Game over")
+                    continue
+                r, c, o = nm
+                answer = {
+                    'type': 'action',
+                    'location': [r, c],
+                    'orientation': o
+                }
             else:
-                logger.error("Unknown message type:\n{}".format(msg))
+                # Wait for the opponent
+                answer = None
 
-            if answer is not None:
-                # print(answer)
-                await websocket.send(json.dumps(answer))
-                logger.info("> {}".format(answer))
-    except websockets.exceptions.ConnectionClosed as err:
-        logger.info("Connection closed")
+        elif msg["type"] == "action":
+            r, c = msg["location"]
+            o = msg["orientation"]
+            agent.register_action(r, c, o, msg["player"])
+            if msg["nextplayer"] == agent.player:
+                agent.update_score(msg['score'])
+                nm = agent.next_action()
+                if nm is None:
+                    logger.info("Game over")
+                    continue
+                nr, nc, no = nm
+                answer = {
+                    'type': 'action',
+                    'location': [nr, nc],
+                    'orientation': no
+                }
+            else:
+                answer = None
+
+        elif msg["type"] == "end":
+            # End the game
+            agent.end_game()
+            answer = None
+        else:
+            logger.error("Unknown message type:\n{}".format(msg))
+
+        if answer is not None:
+            await websocket.send(json.dumps(answer))
+            logger.info("> {}".format(answer))
     logger.info("Exit handler")
 
 
@@ -247,8 +192,6 @@ def main(argv=None):
     logger.setLevel(
         max(logging.INFO - 10 * (args.verbose - args.quiet), logging.DEBUG))
     logger.addHandler(logging.StreamHandler(sys.stdout))
-
-    agentclass = DotsAndBoxesAgent
     start_server(args.port)
 
 
